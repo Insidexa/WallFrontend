c93193d8e80ac4ebe167e9ffe84d9db44f9e088e
jashka <jashka@localhost.localdomain>
2016-10-31 15:13:44 +0300

    implements update comment

--- components/wall/wall.controller.js
+++ components/wall/wall.controller.js
@@ -1,19 +1,18 @@
 
-WallController.$inject = ['$scope', 'WSFactory', '$timeout'];
-function WallController($scope, WSFactory, $timeout) {
+WallController.$inject = ['$scope', 'WSFactory', '$timeout', 'orderByFilter'];
+function WallController($scope, WSFactory, $timeout, orderByFilter) {
     var vm = this;
 
     vm.wall = {
         images: []
     };
     vm.walls = [];
-    vm.comment = {
-        parent_id: 0
-    };
     vm.server = SERVER_URL;
     vm.flowObject = {
         files: []
     };
+    vm.reverse = true;
+    vm.propertyName = 'created_at';
 
     vm.init = init;
     vm.filesAdded = filesAdded;
@@ -24,9 +23,42 @@ function WallController($scope, WSFactory, $timeout) {
     vm.edit = edit;
     vm.like = like;
     vm.noInteresting = noInteresting;
-    
+    vm.sortBy = sortBy;
+    vm.editCommentForm = editCommentForm;
+    vm.removeComment = removeComment;
+    vm.updateComment = updateComment;
+
+    function removeComment(wallIndex, commentIndex) {
+        WSFactory.send({
+            action: 'user_remove_comment',
+            wall_id: vm.walls[wallIndex].id,
+            comment_id: vm.walls[wallIndex].comments[commentIndex].id
+        });
+    }
+
+    function editCommentForm(wallIndex, commentIndex) {
+        vm.comment = vm.walls[wallIndex].comments[commentIndex];
+    }
+
+    function updateComment(comment) {
+        WSFactory.send({
+            action: 'user_update_comment',
+            comment: comment
+        });
+    }
+
+    function sortBy(propertyName) {
+        vm.reverse = (propertyName !== null && vm.propertyName === propertyName)
+            ? !vm.reverse : false;
+        vm.propertyName = propertyName;
+        vm.walls = orderByFilter(vm.walls, vm.propertyName, vm.reverse);
+    }
+
     function noInteresting(index) {
-        
+        WSFactory.send({
+            action: 'user_no_interesting',
+            id: vm.walls[index].id
+        });
     }
 
     function like(typeId, type, wallId) {
@@ -45,7 +77,6 @@ function WallController($scope, WSFactory, $timeout) {
                 action: 'get_walls'
             });
         }, function (event) {
-
             var routes = {
                 'client_walls': function (response) {
                     vm.walls = response;
@@ -77,6 +108,35 @@ function WallController($scope, WSFactory, $timeout) {
                             wall.comments.push(response);
                         }
                     })
+                },
+                'client_no_interesting': function (response) {
+                    angular.forEach(vm.walls, function (wall, index) {
+                        if (wall.id == response) {
+                            vm.walls.splice(index, 1);
+                        }
+                    })
+                },
+                'client_remove_comment': function (response) {
+                    angular.forEach(vm.walls, function (wall, wallIndex) {
+                        if (response.wall_id == wall.id) {
+                            angular.forEach(wall.comments, function (comment, commentIndex) {
+                                if (comment.id == response.comment_id) {
+                                    vm.walls[wallIndex].comments.splice(commentIndex, 1);
+                                }
+                            });
+                        }
+                    });
+                },
+                'client_update_comment': function (response) {
+                    angular.forEach(vm.walls, function (wall, wallIndex) {
+                        if (response.wall_id == wall.id) {
+                            angular.forEach(wall.comments, function (comment, commentIndex) {
+                                if (comment.id == response.id) {
+                                    vm.walls[wallIndex].comments[commentIndex] = response;
+                                }
+                            });
+                        }
+                    });
                 }
             };
 
@@ -86,7 +146,7 @@ function WallController($scope, WSFactory, $timeout) {
                 $timeout(function () {
                     routes[data.action](data.response);
                 }, 0);
-            }
+            } else throw new Error('Undeclared route: ', data.action);
         });
     }
 
@@ -106,13 +166,15 @@ function WallController($scope, WSFactory, $timeout) {
         vm.comment.parent_id = commentId;
     }
     
-    function addComment(id) {
+    function addComment(id, text) {
         WSFactory.send({
             action: 'user_add_comment',
-            comment: vm.comment,
+            comment: {
+                text: text,
+                parent_id: 0
+            },
             wall_id: id
         });
-        vm.comment = {parent_id: 0};
     }
 
     function add() {
--- components/wall/wall.module.js
+++ components/wall/wall.module.js
@@ -15,6 +15,54 @@ angular.module('Wall', [])
         return {
             templateUrl: 'components/wall/add.html'
         }
+    })
+    .directive('editComment', function () {
+        return {
+            restrict: 'E',
+            template:  '<div ng-show="commentShow" class="col-md-offset-1 col-md-11">'  +
+            '     <textarea ng-model="editComment.text" class="form-control" rows="1"></textarea>'  +
+            '     <button ng-click="update()" type="button"'  +
+            '     class="btn btn-primary btn-sm">update</button>'  +
+            '  <button ng-click="commentShow = false" class="btn btn-primary btn-sm ">cancel</button>'  +
+            '</div>' ,
+            scope: {
+                comment: '=',
+                callback: '=',
+                commentShow: '=commentShow'
+            },
+            controller: ['$scope', function ($scope) {
+                $scope.update = update;
+                $scope.editComment = angular.copy($scope.comment);
+                
+                function update() {
+                    $scope.commentShow = false;
+                    $scope.callback($scope.editComment);
+                }
+            }]
+        }
+    })
+    .directive('postContent', function () {
+        return {
+            restrict: 'E',
+            template:  '<div class="col-md-12">'  +
+            '     <div class="row">'  +
+            '         <div class="text col-md-12">{{post.text}}</div>'  +
+            '         <div class="images col-md-12">'  +
+            '             <div class="row">'  +
+            '                 <div ng-repeat="image in post.images" class="col-xs-8 col-md-4">'  +
+            '                     <a class="thumbnail">'  +
+            '                         <img src="{{imageBasePath + image.path}}">'  +
+            '                     </a>'  +
+            '                 </div>'  +
+            '             </div>'  +
+            '         </div>'  +
+            '     </div>'  +
+            '</div>',
+            scope: {
+                post: '=post',
+                imageBasePath: '=imageBasePath'
+            }
+        }
     });
 
 ConfigWall.$inject = ['$routeProvider'];
